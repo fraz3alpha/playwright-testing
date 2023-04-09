f9e0eaf1873780963528a4b99eab8bebdf6d4b6b
Andy Taylor <andrewtaylor2@gmail.com>
2023-04-09 15:10:16 +0100

    add extension

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,59 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+// const { test, expect } = require('@playwright/test');
+
+// Some more help taken from https://www.petroskyriakou.com/how-to-load-a-chrome-extension-in-playwright
+// https://playwright.dev/docs/chrome-extensions hasn't been particularly helpful
+
+const { test: base, expect, chromium } = require('@playwright/test')
+const path = require('path')
+
+const extensionPath = path.join(__dirname, '../build') // make sure this is correct
+
+const test = base.extend({
+  context: async ({ browserName }, use) => {
+    const browserTypes = { chromium }
+    const launchOptions = {
+      devtools: true,
+      headless: false,
+      args: [
+        // `--headless=new`,
+        `--disable-extensions-except=${extensionPath}`,
+        `--load-extension=${extensionPath}`
+      ],
+      viewport: {
+        width: 1920,
+        height: 1080
+      }
+    }
+    const context = await browserTypes[browserName].launchPersistentContext(
+      '',
+      launchOptions
+    )
+    await use(context)
+    await context.close()
+  }
+})
+
+// await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.parkrun.org.uk/parkrunner/1309364/all/');
+
+
+  // This takes a screenshot of the entire page, which is probably a good idea to do early on,
+  // but we should really wait until the extension has loaded.
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playwright/, { timeout: 30000 });
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*intro/);
+// });
