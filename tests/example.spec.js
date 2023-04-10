3d06d1ca65f00a57c501b7ae5bb0c5d837f0da7f
Andy Taylor <andrewtaylor2@gmail.com>
2023-04-10 20:42:18 +0100

    Serve parkrunner pages from mocked API endpoint

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,6 +7,8 @@
 const { test: base, expect, chromium } = require('@playwright/test')
 const path = require('path')
 
+const fs = require('fs');
+
 const countryDomain = process.env.COUNTRY_HOSTNAME ? process.env.COUNTRY_HOSTNAME : "parkrun.org.uk"
 
 const extensionPath = path.join(__dirname, '../build') // make sure this is correct
@@ -34,19 +36,69 @@ const test = base.extend({
       '',
       launchOptions
     )
+    // I don't know what this use does.
     await use(context)
+
+    // Mock a route
+    // This doesn't appear to work
+    // await context.route("https://www.parkrun.org.uk/parkrunner/1309364/all/", route => route.fulfill({
+    //   status: 200,
+    //   body: "some text",
+    // }));
+
+    // No idea why we call close here
     await context.close()
   }
 })
 
 // await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
-test('has title', async ({ page }) => {
+test('Basic extension operation', async ({ page }) => {
+
+  // Mock a route.
+  // This does work!
+  await page.route(`https://*/parkrunner/*/all/`, route => {
+
+    const urlRegex = /^https:\/\/www\.(?<parkrunDomain>.*?)\/parkrunner\/(?<parkrunnerId>\d+)\/all\/$/
+    const re = new RegExp(urlRegex)
+    let url = route.request().url()
+    console.log(url)
+    let urlMatch = url.match(urlRegex)
+    console.log(urlMatch)
+
+    if (urlMatch === null || urlMatch === undefined) {
+
+      route.fulfill({
+        status: 404,
+        contentType: 'text/plain',
+        body: "not found"
+      })
+
+    } else {
+
+      const data = fs.readFileSync(
+        `./supporting-data/sites/${urlMatch.groups.parkrunDomain}/contents/parkrunner/${urlMatch.groups.parkrunnerId}/all/index.html`,
+        {
+          encoding:'utf8',
+          flag:'r'
+        }
+      );
+
+      route.fulfill({
+        status: 200,
+        contentType: 'text/html',
+        body: data
+      })
+
+    }
+  }
+  );
+
   await page.goto(`https://www.${countryDomain}/parkrunner/1309364/all/`);
 
-  // Wait 3 seconds, this should be plenty as we are serving all the data locally and there shoudn't be
+  // Wait half a second, this should be plenty as we are serving all the data locally and there shoudn't be
   // any internet calls
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(500);
   
   // Expect a title "to contain" a substring, this probably won't work on anything other than english language sites.
   // await expect(page).toHaveTitle(/results/, { timeout: 1000 });
